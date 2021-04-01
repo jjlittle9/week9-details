a5d7a6d507e5ff33fcd993b89b1051ff02432c96
Jeffrey Little <dcjeffreydc@gmail.com>
2021-04-01 16:51:17 -0500

    Can't populate information on page

---
+++ src/app/detail/detail.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail.page.html',
+  styleUrls: ['./detail.page.scss'],
+})
+export class DetailPage implements OnInit {
+
+  public ship:any[] = [];
+
+  constructor(private route: ActivatedRoute, private httpClient: HttpClient) { }
+
+  ngOnInit() {
+    this.getStarWarsDetails();
+  }
+
+  getStarWarsDetails() {
+
+    const starWarsId = +this.route.snapshot.paramMap.get('id');
+  
+    this.httpClient.get(`https://swapi.dev/api/starships/${starWarsId}`).subscribe( (ship: any) => {
+      console.log(ship);
+    });
+  }
+
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  public ships: any[] = []; 
+  public cachedShips: any[] = [];
+  
+  public searchTerm: string = "";
 
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+
+    this.httpClient.get('https://swapi.dev/api/starships/').subscribe((apiData: any) => {      
+      
+      const shipsWithIds = apiData.results.map( (ship: any, index: number) => {
+        ship.id = index + 1
+        return ship
+      })
+
+      this.ships = shipsWithIds;
+    })
+  }
+
+  searchForAShip() {
+
+    if(this.searchTerm === "" || this.searchTerm === null) {
+      this.ships = this.cachedShips;
+      
+      this.cachedShips = null
+
+      return
+    }
+
+    this.cachedShips = this.ships;
+
+    this.ships = this.ships.filter( (ship: any) => {
+      return ship.name.toLowerCase().includes(this.searchTerm.toLowerCase());
+    })
+  }
 }
